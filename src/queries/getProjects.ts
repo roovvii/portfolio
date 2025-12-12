42b374605073bedb9000962fb929b3dd9e5f7774
roovvii <ravi.palavai@gmail.com>
2025-12-11 22:11:10 -0600

    Use static projectsContent instead of DatoCMS

--- src/queries/getProjects.ts
+++ src/queries/getProjects.ts
@@ -1,21 +1,20 @@
-// queries/getProjects.ts
-import datoCMSClient from './datoCMSClient';
+// src/queries/getProjects.ts
+
 import { Project } from '../types';
+import { projectsContent } from './localContent';
 
-const GET_PROJECTS = `
-  query {
-    allProjects(orderBy: title_ASC) {
-      title
-      description
-      techUsed
-      image {
-        url
-      }
-    }
-  }
-`;
-
+// We no longer fetch from DatoCMS. Instead, we map static project entries
+// from localContent into the Project shape the UI expects.
 export async function getProjects(): Promise<Project[]> {
-  const data = await datoCMSClient.request<{ allProjects: Project[] }>(GET_PROJECTS);
-  return data.allProjects;
+  return projectsContent.map((proj) => ({
+    title: proj.title,
+    description: proj.description,
+    // You can later customize this per project in localContent if you want.
+    // For now we leave it empty, which just means "no tech badges" or a minimal display.
+    techUsed: '',
+    // You can later point this to a real image in /public or an external URL.
+    image: {
+      url: '',
+    },
+  }));
 }
